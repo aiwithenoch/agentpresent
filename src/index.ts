968310ae302e2976ddd01aeb42a524d816bf9aa5
Ai With Enoch <aiwithenoch@gmail.com>
2026-08-03 22:29:16 +0000

    feat: export public AgentPresent API

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export { AgentPresent, haversineMeters } from "./engine.js";
+export type { AgentPresentOptions } from "./engine.js";
+export { systemClock } from "./providers.js";
+export type {
+  Clock,
+  LocationProvider,
+  PlaceProvider,
+  ReminderNotifier,
+  RouteProvider,
+} from "./providers.js";
+export type {
+  Coordinates,
+  Destination,
+  LocationSnapshot,
+  ReminderEvent,
+  ReminderIntent,
+  ReminderState,
+  RouteEstimate,
+} from "./types.js";
