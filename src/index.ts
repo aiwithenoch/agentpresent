98e702d047db84c0f25ff0389023946d92f6fb65
Ai With Enoch <aiwithenoch@gmail.com>
2026-08-03 22:53:48 +0000

    feat: export resilient monitoring APIs

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,19 @@ export type { AgentPresentOptions } from "./engine.js";
 export { systemClock } from "./providers.js";
 export type {
   Clock,
+  ErrorClassifier,
   LocationProvider,
   PlaceProvider,
   ReminderNotifier,
   RouteProvider,
+  TelemetrySink,
 } from "./providers.js";
 export type {
   Coordinates,
   Destination,
   LocationSnapshot,
+  MonitorHandle,
+  MonitorResult,
   ReminderEvent,
   ReminderIntent,
   ReminderState,
